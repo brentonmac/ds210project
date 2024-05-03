8c2849847fe5d86bbdc7392f05acf803ee9e918c
brentonmac <brentonmac24@gmail.com>
2024-05-03 16:19:23 -0400

    added comments

--- project/src/popular.rs
+++ project/src/popular.rs
@@ -1,7 +1,8 @@
 pub fn popularity_scale(z_scores: Vec<f64>) -> Vec<usize> {
+    // returns a vector of popularity scale, which is based on the z-score of node degree
     let mut scales = vec![];
     for &z in &z_scores {
-        let scale = match z {
+        let scale = match z { // matching each level of z-score with a popularity scale
             x if x < -2.0 => 1,
             x if x < -1.0 => 2,
             x if x < 0.0 => 3,
@@ -16,9 +17,10 @@ pub fn popularity_scale(z_scores: Vec<f64>) -> Vec<usize> {
 }
 
 pub fn celeb(pop_scale: Vec<usize>) -> Vec<usize> {
+    //returns a vector of the nodes that are considered celebrities, which are people with a 7 on popularity scale
     let mut celebs = vec![];
     for i in 0..pop_scale.len() {
-        if pop_scale[i] == 7 {
+        if pop_scale[i] == 7 { // if the popularity score is 7, push that node to the vector
             celebs.push(i);
         }
     } 
--- project/src/stats.rs
+++ project/src/stats.rs
@@ -1,47 +1,50 @@
-pub struct Stats {
+pub struct Stats { //struct of stats
     pub data: Vec<usize>,
 }
 
-impl Stats {
-    pub fn new(data: Vec<usize>) -> Stats {
+impl Stats { // implements Stats struct into many different methods
+    pub fn new(data: Vec<usize>) -> Stats { // created a Stats struct
         Stats {data }
     }
 
     pub fn descriptive_stats(&self) -> Vec<f64> {
-        let mut sorted_data = self.data.clone();
+        //outputs the minimum, Q1, median, Q3, and maximum of the data
+        let mut sorted_data = self.data.clone(); // cloning the data so the sort below does not affect the original data
         sorted_data.sort();
-    
         let n = sorted_data.len();
         let mut stats = vec![0.0; 5];
-        stats[0] = sorted_data[0] as f64;
-        stats[1] = sorted_data[n / 4] as f64;
-        if n % 2 == 0 {
+        stats[0] = sorted_data[0] as f64; //first stat in the desc stats is the minimum, or the first value in the sorted data
+        stats[1] = sorted_data[n / 4] as f64; 
+        if n % 2 == 0 { //If there is an odd len, the median is the average of the middle point and the point before it
             stats[2] = (sorted_data[n / 2 - 1] + sorted_data[n / 2]) as f64 / 2.0;
         } else {
             stats[2] = sorted_data[n / 2] as f64;
         }
-        if n % 4 == 0 {
+        if n % 4 == 0 { //If the len is not divisble by 4, Q3 = the average of the 75% percentile point and the point before that
             stats[3] = (sorted_data[3 * n / 4 - 1] + sorted_data[3 * n / 4]) as f64 / 2.0;
         } else {
             stats[3] = sorted_data[3 * n / 4] as f64;
         }
-        stats[4] = sorted_data[n - 1] as f64;
+        stats[4] = sorted_data[n - 1] as f64; // The last point is the max
         stats
     }
 
     pub fn mean(&self) -> f64 {
+        //finds the mean of the data by summing the values and dividing by the length
         let sum: usize = self.data.iter().sum();
         let mean = sum as f64 / self.data.len() as f64;
         mean
     }
 
     pub fn stdev(&self) -> f64 {
+        //finds the standard deviation of the data
         let m = self.mean();
         let variance = self.data.iter().map(|&x| (x as f64 - m).powi(2)).sum::<f64>() / self.data.len() as f64;
         variance.sqrt()
     }
 
     pub fn zscores(&self) -> Vec<f64> {
+        //finds the z-score of each data point based on the mean and standard deviation
         if self.data.is_empty() {
             return Vec::new();
         }
@@ -49,9 +52,8 @@ impl Stats {
         let sd = self.stdev();
         let mut scores = vec![];
         for i in &self.data {
-            scores.push(((*i as f64)-m)/sd);
+            scores.push(((*i as f64)-m)/sd); // z-score is data - mean / standard deviation
         }
         scores
     }
-    
 }
